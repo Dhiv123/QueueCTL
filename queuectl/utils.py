b045108a9492147254193c234870ff461ab6fcd6
Dhiv123 <dhiv24arcade@gmail.com>
2025-11-09 13:20:44 +0530

    Update utils.py

--- queuectl/utils.py
+++ queuectl/utils.py
@@ -1,9 +1,9 @@
 import datetime
 
 def now_ts():
-    """Return current UTC timestamp as float (seconds)."""
+    # Return current UTC timestamp as float
     return datetime.datetime.utcnow().timestamp()
 
 def iso_now():
-    """Return current UTC time in ISO 8601 format (string)."""
+    # Return current UTC time in ISO format
     return datetime.datetime.utcnow().isoformat()
